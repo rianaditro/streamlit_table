67e469f1c1ba670db9e66a141a29e6e714c9828b
rianaditro <rianaditro@gmail.com>
2024-05-18 08:44:32 +0700

    add upload info

--- pages/table_module4.py
+++ pages/table_module4.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 from io import StringIO
+from datetime import datetime
 
 from converter import extract_module_4
 
@@ -15,4 +17,9 @@ upload = st.file_uploader('Pilih File', type='txt', accept_multiple_files=False)
 if upload:
     txt = StringIO(upload.getvalue().decode("utf-8")).readlines()
     module_4_df = extract_module_4(txt)
-    st.dataframe(module_4_df, use_container_width=True)
+    module_4_df = pd.DataFrame(module_4_df)
+    upload_datetime = datetime.now()
+    upload_perangkat = perangkat
+    upload_ip = perangkat_df[perangkat_df['nama_perangkat'] == perangkat]['ip'].values[0]
+    module_4_df = module_4_df.assign(upload_datetime=upload_datetime, upload_perangkat=upload_perangkat, upload_ip=upload_ip)
+    st.dataframe(module_4_df, use_container_width=True, hide_index=True)
